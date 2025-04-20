22410188873a041e950865092977e0f04851180d
rana <masudur373515@gmail.com>
2025-04-20 17:06:10 +0600

    test

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let Fruits = prompt("Please Enter the fruit Name:")
+
+switch (Fruits) {
+    case "Apple":
+        document.write("Apple Is Teats fruits");
+        break;
+    case "Orange":
+        document.write("Orange Vaitamin C++");
+        break;
+    case "Mango":
+        document.write("Mango Is green fruits");
+        break;
+    case "gwaba":
+        document.write("gwaba is bad fruits")
+        break;
+}
+    
