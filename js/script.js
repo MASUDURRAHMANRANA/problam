1185412839673ebfb957c88599cea920a8ec01fc
Your Name <your.email@example.com>
2025-04-21 20:21:01 +0600

    first message

--- js/script.js
+++ js/script.js
@@ -11,7 +11,10 @@ switch (Fruits) {
         document.write("Mango Is green fruits");
         break;
     case "gwaba":
-        document.write("gwaba is bad fruits")
+        document.write("gwaba is bad fruits");
+        break;
+    case "watermelon":
+            document.write("Watermelons are large, juicy, sweet fruits, typically round or oblong, with a hard green rind and red or pink flesh. They are a popular summer treat and can be enjoyed fresh, in salads, or in smoothies. While commonly used as a fruit, watermelon is also a member of the gourd family (Cucurbitaceae) and can be considered a vegetable as well. ");
         break;
 }
     
